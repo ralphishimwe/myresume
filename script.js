cacaba19b36dd83efbb3b06df072dc6af5838cc8
Ralph <r.ishimwe3@alustudent.com>
2025-06-15 13:08:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function toggleMenu() {
       const name = document.getElementById('name').value.trim();
       const email = document.getElementById('email').value.trim();
       const message = document.getElementById('message').value.trim();
-      const emailRegex = /^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$/;
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
       if (!name || !email || !message) {
         alert('All fields are required.');
